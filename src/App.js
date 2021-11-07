1de6d2cc9a419223dd5b4a32bfead8ded2c0efa7
Dylan Shoemaker <me@dylanshoemaker.com>
2021-11-06 19:03:59 -0700

    add navbar to about

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import './App.css';
+import Nav from './components/Nav';
 import About from './components/About';
 
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <main>
-        <About/>
+        <About></About>
       </main>
     </div>
   );
