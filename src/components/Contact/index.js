8d26bf055a300170fae2868f280219bfb41b107a
Dylan Shoemaker <me@dylanshoemaker.com>
2021-11-07 18:17:28 -0800

    add useState hook to ContactForm component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ContactForm() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = formState;
+  function handleChange(e) {
+    setFormState({...formState, [e.target.name]: e.target.value });
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+  //In the preceding function, we're using the setFormState function to update the formState value for the name property. 
+  //We assign the value taken from the input field in the UI with e.target.value and assign this value to the property formState.name. 
+  //We use the spread operator, ...formState, so we can retain the other key-value pairs in this object. 
+  //Without the spread operator, the formState object would be overwritten to only contain the name: value key pair.
+  console.log(formState);
   return (
     <section>
       <h1>Contact me</h1>
-      <form id="contact-form">
+      <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" />
+          <input
+            type="text"
+            defaultValue={name}
+            onChange={handleChange}
+            name="name"
+          />
         </div>
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" />
+          <input type="email" defaultValue={email} name="email" />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" />
+          <textarea name="message" defaultValue={message} rows="5" />
         </div>
         <button type="submit">Submit</button>
       </form>
